59ba2958dc30391a80fd443e3f19e84f627d6c0e
fzhou1010 <165624165+fzhou1010@users.noreply.github.com>
2024-06-25 20:39:34 -0600

    accidentally replaced old app

---
+++ App.js
@@ -0,0 +1,63 @@
+import "./App.css";
+import { useEffect, useState } from 'react';
+import { Spinner } from "react-bootstrap";
+import DiningHall from "./DiningHall/DiningHall.js";
+import LoginPage from "./UserPage/LoginPage.js";
+
+function App() {
+  const [locations, setLocations] = useState([]);
+  const [fullMenu, setFullMenu] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('/data/getMenu')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setFullMenu(data);
+        const locs = data.map((el) => el.Location).sort();
+        setLocations(locs);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setError(e.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <LoginPage />
+
+      {loading ? (
+        <div className="mobile-box">
+          <div className="flex-center">
+            <p className="fs-5 title">Retrieving today's menus</p>
+            <Spinner className="spinner" style={{ color: "#FFFFFF" }} animation="border" role="status" />
+          </div>
+        </div>
+      ) : error ? (
+        <div className="mobile-box">
+          <div className="flex-center">
+            <p className="fs-5 title text-danger">Error retrieving menu: {error}</p>
+            <p className="fs-5 title text-danger">Please try reloading</p>
+          </div>
+        </div>
+      ) : (
+        <div className="intro-fade">
+          <div className="mobile-box">
+            <DiningHall locations={locations} fullMenu={fullMenu} />
+          </div>
+        </div>
+
+      )}
+    </>
+  );
+}
+
+export default App;
